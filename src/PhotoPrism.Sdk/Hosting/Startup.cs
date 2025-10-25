9b33ca58a8a5040c52c58538e9d5d88502c0b70b
matrumz <matrumz@hotmail.com>
2025-10-25 16:12:47 +0000

    feat: register PP.Sdk startup with Web builder
    Issues:
    - #17
    - #21

---
+++ src/PhotoPrism.Sdk/Hosting/Startup.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PhotoPrism.Sdk.Hosting;
+
+public static class Startup
+{
+
+    public static void ConfigureServices(IServiceCollection services) => services
+        .AddHttpClient("PhotoPrism", client =>
+        {
+            var baseUrl = Environment.GetEnvironmentVariable("PHOTOPRISM_SITE_URL") ?? "http://host.docker.internal:2342";
+            client.BaseAddress = new Uri(baseUrl);
+        })
+        ;
+
+}
